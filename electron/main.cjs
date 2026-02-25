cfdf0583487ebfb4b4d7db3b86870badd4161e86
Med-dev-sl <mohamedsallu24@gmail.com>
2026-02-25 02:20:07 +0000

    Build: Setup Electron for Windows Desktop support and build workflow

---
+++ electron/main.cjs
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        title: "UNI AI",
+        icon: path.join(__dirname, '../public/logo.png'),
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+
+    if (isDev) {
+        win.loadURL('http://localhost:5173');
+    } else {
+        win.loadFile(path.join(__dirname, '../dist/index.html'));
+    }
+
+    // Remove menu bar for a cleaner look
+    win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
